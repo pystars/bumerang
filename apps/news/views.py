35b78bb8c6e51787d114b5242e0110b7b5aae6de
Алексей <alexei.iljin@gmail.com>
2011-12-07 20:53:34 +0400

    Added pagination to News

--- apps/news/views.py
+++ apps/news/views.py
@@ -6,8 +6,10 @@ from apps.news.models import *
 
 class NewsRootView(ListView):
     template_name = "news.html"
-    object_list = NewsItem.objects.all()
+    object_list = NewsItem.objects.all().order_by('-creation_date')
     categories = NewsCategory.objects.all()
+
+    paginate_by = 10
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(
