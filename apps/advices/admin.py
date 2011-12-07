f4015dd99d18de09a4c0bddca4cbabd768301908
Алексей <alexei.iljin@gmail.com>
2011-12-07 18:02:36 +0400

    Added TinyMCE support for Advices app in admin panel.

--- apps/advices/admin.py
+++ apps/advices/admin.py
@@ -8,5 +8,10 @@ from apps.advices.models import *
 class AdviceAdmin(MPTTModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
+    class Media:
+        js = [
+            'grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js',
+            'j/tinymce_setup.js',
+        ]
 
 admin.site.register(Advice, AdviceAdmin)
