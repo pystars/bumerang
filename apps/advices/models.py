bd5c4b8095aa30d28eb6bc2117ea9eda181b9b4a
Алексей <alexei.iljin@gmail.com>
2011-12-17 20:42:26 +0400

    Changes to Advices

--- apps/advices/models.py
+++ apps/advices/models.py
@@ -8,7 +8,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Advice(MPTTModel):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=255, unique=True)
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
     description = models.TextField(verbose_name=u'Описание раздела', default=u'Введите описание')
     # Slug of node's name
