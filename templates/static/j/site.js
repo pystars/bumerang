811ea1f1ed46e395ed32afc8193cd32d53abd530
alexey <alexey@goodfellow-laptop.(none)>
2012-01-09 16:31:04 +0400

    Added cross-browser JSON serialization\deserialization

--- templates/static/j/site.js
+++ templates/static/j/site.js
@@ -1,5 +1,36 @@
+// implement JSON.stringify serialization
+var JSON = JSON || {};
+JSON.stringify = JSON.stringify || function (obj) {
+    var t = typeof (obj);
+    if (t != "object" || obj === null) {
+        // simple data type
+        if (t == "string") obj = '"'+obj+'"';
+        return String(obj);
+    }
+    else {
+        // recurse array or object
+        var n, v, json = [], arr = (obj && obj.constructor == Array);
+        for (n in obj) {
+            v = obj[n]; t = typeof(v);
+            if (t == "string") v = '"'+v+'"';
+            else if (t == "object" && v !== null) v = JSON.stringify(v);
+            json.push((arr ? "" : '"' + n + '":') + String(v));
+        }
+        return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+    }
+};
+// implement JSON.parse de-serialization
+JSON.parse = JSON.parse || function (str) {
+    if (str === "") str = '""';
+    eval("var p=" + str + ";");
+    return p;
+};
+
+
+// Site variables
 var delay_time = 4000;
 
+// Site handlers
 $(function(){
     // Messages
     $('.msg-close').click(function(){
@@ -24,81 +55,4 @@ $(function(){
             aspectRatio: 1
         });
     });
-
-
-
-//    // Avatar
-//    jQuery(function($) {
-//        $('#cvs').Jcrop({
-//            //onSelect:    showCoords,
-//            bgColor:     'black',
-//            bgOpacity:   .5,
-//            minSize: [100, 100],
-//            setSelect:   [ 100, 100, 50, 50 ],
-//            aspectRatio: 1
-//        });
-//    });
-//
-//    $('#id_avatar[type=file]').change(function(event){
-//
-//        var file = event.target.files[0];
-//        var i = $(file);
-//
-//
-//        var reader = new FileReader();
-//
-//        var oFReader = new FileReader();
-//        oFReader.readAsDataURL(event.target.files[0]);
-//        oFReader.onload = function (oFREvent) {
-//
-//            var img = new Image();
-//            img.onload = function () {
-//                context.drawImage(this, 0, 0);
-//            }
-//            img.src = oFREvent.target.result;
-//
-//            console.log(img.width);
-//
-//            //document.getElementById("uploadPreview").src = oFREvent.target.result;
-//            $('.jcrop-holder img').attr('src', oFREvent.target.result);
-//
-//        };
-//
-//
-//        reader.onload = (function(theFile) {
-//            return function(e) {
-//                // Render thumbnail.
-//                //var imgHTML = '<img class="file-input-thumb" src="' +  + '" title="' + theFile.name + '"/>';
-//                //$('#asd').attr('src', e.target.result);
-//                var i = new Image();
-//                i.src = e.target.result;
-//                console.log(i.width);
-//
-//                var i = $('<img>').attr('src', e.target.result);
-//                console.log(i.width());
-//
-//                //i.src = e.target.result;
-//                //console.log(i.fileSize);
-//                $('.jcrop-holder img').attr('src', e.target.result);
-////                if( typeof params.selector != 'undefined' ){
-////                    $(params.selector).html(imgHTML);
-////                }else{
-////                    fileInput.before(imgHTML);
-////                }
-//            };
-//        })(file);
-//
-//        // Read in the image file as a data URL.
-//        //reader.readAsDataURL(file);
-//
-//
-//    });
-//    $('#asd').Jcrop({
-//        //onSelect:    showCoords,
-//        bgColor:     'black',
-//        bgOpacity:   .5,
-//        minSize: [100, 100],
-//        setSelect:   [ 100, 100, 50, 50 ],
-//        aspectRatio: 1
-//    });
 })
