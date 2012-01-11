4371df20c15323e3e2cfe7d2c6095283d91249eb
alexey <alexey@goodfellow-laptop.(none)>
2012-01-11 17:32:16 +0400

    JCrop added

--- apps/bumerang_site/views.py
+++ apps/bumerang_site/views.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.views.generic import ListView
+from django.views.generic.base import TemplateView
 
-class BumerangIndexView(ListView):
+class BumerangIndexView(TemplateView):
     template_name = "index.html"
-    queryset = {}
+
+    def get_context_data(self, **kwargs):
+        ctx = super(BumerangIndexView, self).get_context_data(**kwargs)
+        ctx.update({
+
+        })
+        return ctx
