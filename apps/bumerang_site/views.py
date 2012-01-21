e1955ff83a644bd67db41d628936159b1efb7022
clincher <va.bolshakov@gmail.com>
2012-01-21 20:58:04 +0400

    main page fixed

--- apps/bumerang_site/views.py
+++ apps/bumerang_site/views.py
@@ -1,31 +1,23 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-from django.core.exceptions import ObjectDoesNotExist
-
 from django.views.generic.base import TemplateView
+
 from apps.video.models import PlayList, Channel
+
 
 class BumerangIndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         ctx = super(BumerangIndexView, self).get_context_data(**kwargs)
-
         main_channel = Channel.objects.get(slug='main')
         try:
-            ctx.update({'playlist': PlayList.objects.filter(
-                channel=main_channel, rotate_till__gte=datetime.now()).get()})
+            playlist = PlayList.objects.filter(
+                channel=main_channel, rotate_till__gte=datetime.now())[0]
         except PlayList.DoesNotExist:
-            pass #no playlist
-
-        try:
-            main_channel = Channel.objects.get(slug='main')
-            playlist = PlayList.objects.get(channel=main_channel)
-        except ObjectDoesNotExist:
-            playlist = False
-
-        ctx.update({
-            'playlist': playlist,
-        })
+            playlist = None
+        except IndexError:
+            playlist = None
+        ctx.update({'playlist': playlist})
         return ctx
