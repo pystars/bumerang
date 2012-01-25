d971898cffad04d21c1787b4a9f769f012d6d9c6
jessey <alexei.iljin@gmail.com>
2012-01-25 21:18:07 +0400

    Хранилище файлов теперь переписывает файл при совпадении названия

--- apps/video/models.py
+++ apps/video/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from hashlib import sha1
-from django.db.models.query import QuerySet
+from django.core.files import locks
+from django.core.files.move import file_move_safe
+from django.core.files.storage import FileSystemStorage
+import errno
+from django.utils.encoding import force_unicode
+
 import os
 from datetime import timedelta, datetime
 
@@ -10,6 +15,7 @@ from django.core.urlresolvers import reverse
 from apps.video.mediainfo import ExecutionError
 
 from mediainfo import get_metadata
+from django.conf import settings
 
 nullable = dict(null=True, blank=True)
 
@@ -68,6 +74,79 @@ class VideoFileField(models.FileField):
         return os.path.join(self.get_directory_name(), self.get_filename(''.join([hashname, extension])))
 
 
+class VideoFilesStorage(FileSystemStorage):
+    def save(self, name, content):
+        """
+        Saves new content to the file specified by name. The content should be a
+        proper File object, ready to be read from the beginning.
+        """
+        # Get the proper name for the file, as it will actually be saved.
+        if name is None:
+            name = content.name
+
+        #name = self.get_available_name(name)
+        name = self._save(name, content)
+
+        # Store filenames with forward slashes, even on Windows
+        return force_unicode(name.replace('\\', '/'))
+
+    def _save(self, name, content):
+        full_path = self.path(name)
+
+        directory = os.path.dirname(full_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        elif not os.path.isdir(directory):
+            raise IOError("%s exists and is not a directory." % directory)
+
+        # There's a potential race condition between get_available_name and
+        # saving the file; it's possible that two threads might return the
+        # same name, at which point all sorts of fun happens. So we need to
+        # try to create the file, but if it already exists we have to go back
+        # to get_available_name() and try again.
+
+        while True:
+            try:
+                # This file has a file path that we can move.
+                if hasattr(content, 'temporary_file_path'):
+                    file_move_safe(content.temporary_file_path(), full_path)
+                    content.close()
+
+                # This is a normal uploadedfile that we can stream.
+                else:
+                    # This fun binary flag incantation makes os.open throw an
+                    # OSError if the file already exists before we open it.
+                    # fd = os.open(full_path, os.O_WRONLY | os.O_CREAT | os.O_EXCL | getattr(os, 'O_BINARY', 0))
+
+
+                    fd = os.open(full_path, os.O_WRONLY | os.O_CREAT | getattr(os, 'O_BINARY', 0))
+                    try:
+                        locks.lock(fd, locks.LOCK_EX)
+                        for chunk in content.chunks():
+                            os.write(fd, chunk)
+                    finally:
+                        locks.unlock(fd)
+                        os.close(fd)
+            # Теперь это исключение не возникнет никогда
+            # так как файл открывается на перезапись если существует,
+            # но я решил не нарушать структуру кода
+            except OSError, e:
+                if e.errno == errno.EEXIST:
+                    # Ooops, the file exists. We need a new file name.
+                    name = self.get_available_name(name)
+                    full_path = self.path(name)
+                else:
+                    raise
+            else:
+                # OK, the file save worked. Break out of the loop.
+                break
+
+        if settings.FILE_UPLOAD_PERMISSIONS is not None:
+            os.chmod(full_path, settings.FILE_UPLOAD_PERMISSIONS)
+
+        return name
+
+
 class Video(models.Model):
     ACCESS_FLAGS = (
         (1, u'Всем пользователям'),
@@ -86,7 +165,8 @@ class Video(models.Model):
     title = models.CharField(u'Название', max_length=255)
     slug = models.SlugField(u'Метка (часть ссылки)', **nullable)
     original_file = VideoFileField(u"Оригинальное видео",
-        upload_to='videos/originals', **nullable)
+        upload_to='videos/originals',
+        storage=VideoFilesStorage(), **nullable)
     hq_file = VideoFileField(u'Видео высокого качества',
         upload_to='videos/high', **nullable)
     mq_file = VideoFileField(u'Видео среднего качества',
