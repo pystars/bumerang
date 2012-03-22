3bd344d618c28b5a5bb6f1959b72a9abebfdadad
clincher <va.bolshakov@gmail.com>
2012-03-23 01:59:14 +0400

    some code cleanups. need tests, btw

--- bumerang/apps/video/templatetags/videotags.py
+++ bumerang/apps/video/templatetags/videotags.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 import time
+from datetime import datetime
 
 from django import template
 
 register = template.Library()
 
-@register.filter
-def to_hm(value, is_last=False):
+#@register.filter
+#def to_hm(value, is_last=False):
     #time_val = time.gmtime(int(value.offset/1000))
-    rotate = value.playlist.rotate_from
-    offset_time = rotate.second + rotate.minute*60 + rotate.hour*60**2 + int(value.offset/1000)
-    total_time = time.gmtime(offset_time)
-    if is_last:
-        total_time = time.gmtime(offset_time+int(value.video.duration/1000))
-    return time.strftime('%H:%M', total_time)
+#    rotate = value.playlist.rotate_from
+#    offset_time = rotate.second + rotate.minute*60 + rotate.hour*60**2 + int(value.offset/1000)
+#    total_time = time.gmtime(offset_time)
+#    if is_last:
+#        total_time = time.gmtime(offset_time+int(value.video.duration/1000))
+#    return time.strftime('%H:%M', total_time)
 
 @register.filter
 def to_hms(value):
@@ -27,12 +28,30 @@ def to_hms(value):
 
 @register.filter
 def is_future(value):
-    now = time.localtime()
-    now_secs = now.tm_sec + now.tm_min*60 + now.tm_hour*60**2
-    rotate = value.playlist.rotate_from
-    offset_time = rotate.second + rotate.minute*60 + rotate.hour*60**2 + int(value.offset/1000)
-
-    if offset_time > now_secs:
+    if value.rotate_from() > datetime.now():
         return True
-    else:
-        return False
+    return False
+#    now = time.localtime()
+#    now_secs = now.tm_sec + now.tm_min*60 + now.tm_hour*60**2
+#    rotate = value.playlist.rotate_from
+#    offset_time = rotate.second + rotate.minute*60 + rotate.hour*60**2 + int(value.offset/1000)
+#
+#    if offset_time > now_secs:
+#        return True
+#    else:
+#        return False
+
+@register.filter
+def is_now(value):
+    if value.rotate_from() < datetime.now() < value.rotate_till():
+        return True
+    return False
+#    now = time.localtime()
+#    now_secs = now.tm_sec + now.tm_min*60 + now.tm_hour*60**2
+#    rotate = value.playlist.rotate_from
+#    offset_time = rotate.second + rotate.minute*60 + rotate.hour*60**2 + int(value.offset/1000)
+#
+#    if offset_time > now_secs:
+#        return True
+#    else:
+#        return False
