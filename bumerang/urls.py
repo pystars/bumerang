b22e3137323f787d2788a1a793b8adfcca9e4efa
clincher <va.bolshakov@gmail.com>
2012-05-04 01:01:25 +0400

    ckeditor added

--- bumerang/urls.py
+++ bumerang/urls.py
@@ -22,7 +22,4 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
-
-#    from django.conf.urls.static import static
-
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
