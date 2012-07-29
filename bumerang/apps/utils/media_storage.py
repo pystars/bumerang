eafdeda37c7982a00e9a1181d848a669b3df2a9a
clincher <va.bolshakov@gmail.com>
2012-07-29 16:10:20 +0400

    modified time fixed

--- bumerang/apps/utils/media_storage.py
+++ bumerang/apps/utils/media_storage.py
@@ -17,15 +17,15 @@ class CachedS3BotoStorage(S3BotoStorage):
     """
     S3 storage backend that saves the files locally, too.
     """
-    def __init__(self, *args, **kwargs):
-        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
-        self.local_storage = get_storage_class(
-            "compressor.storage.CompressorFileStorage")()
-
-    def save(self, name, content):
-        name = super(CachedS3BotoStorage, self).save(name, content)
-        self.local_storage._save(name, content)
-        return name
+#    def __init__(self, *args, **kwargs):
+#        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
+#        self.local_storage = get_storage_class(
+#            "compressor.storage.CompressorFileStorage")()
+#
+#    def save(self, name, content):
+#        name = super(CachedS3BotoStorage, self).save(name, content)
+#        self.local_storage._save(name, content)
+#        return name
 
     def modified_time(self, name):
         name = self._normalize_name(self._clean_name(name))
