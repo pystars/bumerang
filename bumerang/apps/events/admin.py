6fb059b128991a298f9f2c3157bcbe3f8d0541ce
clincher <va.bolshakov@gmail.com>
2012-08-13 19:27:25 +0400

    #339 fixed

--- bumerang/apps/events/admin.py
+++ bumerang/apps/events/admin.py
@@ -8,6 +8,7 @@ class EventAdmin(admin.ModelAdmin):
     readonly_fields = ('owner',)
     list_display = ('id', '__unicode__', 'created', 'owner_name', 'owner')
     list_filter = ('is_approved', 'opened')
+    list_display_links = ('__unicode__',)
 
 
 admin.site.register(Event, EventAdmin)
