426bfcf1f4de876b6cc5236305786089c20c0d62
clincher <va.bolshakov@gmail.com>
2014-04-18 19:44:13 +0700

    try to fix bad request

--- bumerang/settings.py
+++ bumerang/settings.py
@@ -339,36 +339,7 @@ except ImportError:
         pass
 
 class BumerSettings(LocalSettingsMixin, S3StaticMixin, CommonSettings):
-
-    @property
-    def INSTALLED_APPS(self):
-        if self.DEBUG:
-            return super(BumerSettings, self).INSTALLED_APPS + ['debug_toolbar']
-        return super(BumerSettings, self).INSTALLED_APPS
-
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.versions.VersionsPanel',
-        'debug_toolbar.panels.timer.TimerPanel',
-        'debug_toolbar.panels.settings.SettingsPanel',
-        'debug_toolbar.panels.headers.HeadersPanel',
-        'debug_toolbar.panels.request.RequestPanel',
-        'debug_toolbar.panels.sql.SQLPanel',
-        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-        'debug_toolbar.panels.templates.TemplatesPanel',
-        'debug_toolbar.panels.cache.CachePanel',
-        'debug_toolbar.panels.signals.SignalsPanel',
-        'debug_toolbar.panels.logging.LoggingPanel',
-        'debug_toolbar.panels.redirects.RedirectsPanel',
-    )
-
-    INTERNAL_IPS = ('127.0.0.1',)
-    DEBUG_TOOLBAR_CONFIG = dict(
-        INTERCEPT_REDIRECTS=False
-    )
-
-    COVERAGE_REPORT_HTML_OUTPUT_DIR = os.path.join(
-        CommonSettings.PROJECT_ROOT, 'coverage')
-
+    pass
 
 class AdminSettings(LocalSettingsMixin, CommonSettings):
     pass
