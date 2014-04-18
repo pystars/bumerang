b94456455d0d34cb2287ecefdca2cb833e3e12af
clincher <va.bolshakov@gmail.com>
2014-04-18 19:33:58 +0700

    try to fix strange bug on production

--- bumerang/apps/video/views.py
+++ bumerang/apps/video/views.py
@@ -90,7 +90,7 @@ class VideoDetailView(VideoMixin, DetailView):
     def get_queryset(self):
         return super(VideoDetailView, self).get_queryset().filter(
             status=self.model.READY).annotate(avg_score=Avg(
-            'participantvideo__participantvideoscore__score'))
+            'participantvideos__participantvideoscore__score'))
 
     def get(self, request, **kwargs):
         response = super(VideoDetailView, self).get(request, **kwargs)
