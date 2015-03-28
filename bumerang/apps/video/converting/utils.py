0b4facc3b07f73bacd8e28e4e6ab40c1b45d22c6
clincher <va.bolshakov@gmail.com>
2015-03-28 19:02:49 +0700

    something wrong...

--- bumerang/apps/video/converting/utils.py
+++ bumerang/apps/video/converting/utils.py
@@ -67,11 +67,12 @@ def convert_original_video(sender, **kwargs):
                 [{'Key': hq_upload_to(video, None),
                  'PresetId': settings.AWS_ELASTICTRANCODER_PRESET}]
             )
-            EncodeJob.objects.create(
+            job = EncodeJob.objects.create(
                 content_type=ContentType.objects.get_for_model(Video),
                 object_pk=video.pk,
                 job_id=encoder.message['Job']['Id']
             )
+            print(job)
 
 
 def update_encode_state(sender, **kwargs):
