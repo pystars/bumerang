0f0901c1349a41aafded96e2edd6d89e0b7763c8
clincher <va.bolshakov@gmail.com>
2015-03-28 17:36:57 +0700

    fix signal

--- bumerang/apps/video/converting/__init__.py
+++ bumerang/apps/video/converting/__init__.py
@@ -4,4 +4,4 @@ from .signals import transcode_onchange
 from .utils import convert_original_video, update_encode_state
 
 file_uploaded.connect(convert_original_video, dispatch_uid='file_uploaded')
-transcode_onchange(update_encode_state)
+transcode_onchange.connect(update_encode_state)
