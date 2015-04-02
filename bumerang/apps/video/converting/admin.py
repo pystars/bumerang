c8c4cfbd505345f0678ed24417dcdac6be1a25aa
clincher <va.bolshakov@gmail.com>
2015-04-03 01:44:39 +0700

    EncodeJob admin changed

--- bumerang/apps/video/converting/admin.py
+++ bumerang/apps/video/converting/admin.py
@@ -4,4 +4,9 @@ from django.contrib import admin
 from models import EncodeJob
 
 
+class EncodeJobAdmin(admin.ModelAdmin):
+    list_display = ['job_id', '__unicode__', 'get_state_display', 'created_at',
+                    'last_modified', 'lead_time']
+    list_display_links = ['job_id', '__unicode__']
+
 admin.site.register(EncodeJob)
